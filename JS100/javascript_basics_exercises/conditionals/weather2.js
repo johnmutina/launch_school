9da9f234abd1462c221bf562dd9b8abf80555b1d
johnmutina <johnmutina@gmail.com>
2019-12-02 17:02:42 +0100

    Wip JS100 exercises

---
+++ JS100/javascript_basics_exercises/conditionals/weather2.js
@@ -0,0 +1,15 @@
+let weather = 'foggy';
+
+switch (weather) {
+  case 'sunny':
+    console.log("It's a beautiful day");
+    break;
+  case 'rainy':
+    console.log('Grab your umbrella');
+    break;
+  case 'foggy':
+    console.log("I can't even see my nose!");
+    break;
+  default:
+    console.log("Let's stay inside");
+}
